350304a2f8242a370fec73b5535a3ecea1a39915
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2026-01-17 23:27:57 -0500

    Java Program 1 and 2

---
+++ Print_Integer.java
@@ -0,0 +1,8 @@
+class Print_Integer{
+    public static void main(String[] args) {
+        int number;
+        for(number = 1; number<=10; number++){
+            System.err.println(number);
+        }
+    }
+}
