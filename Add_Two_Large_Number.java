86bbca391be5341ebe0e92f8dd2e7544279880a9
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2026-01-20 14:36:19 -0500

    Program 8.1

---
+++ Add_Two_Large_Number.java
@@ -0,0 +1,24 @@
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Add_Two_Large_Number{
+    public static void main(String[] args) {
+        String number1, number2;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st number:");
+        number1 = sc.nextLine();
+
+        System.out.println("Enter 2nd number:");
+        number2 = sc.nextLine();
+
+        BigInteger first = new BigInteger(number1);
+        BigInteger second = new BigInteger(number2);
+        BigInteger sum;
+
+        sum = first.add(second);
+
+        System.out.println("sum:" +sum);
+    }
+}
