6b9f2a4bf5ada097d6c0b69fad7209208507850f
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2026-01-21 18:23:35 -0500

    Program 10

---
+++ Condition.java
@@ -0,0 +1,11 @@
+class Condition{
+    public static void main(String[] args) {
+        boolean learning = true;
+
+        if(learning){
+            System.out.println("You are leaning Java.");
+        }else{
+            System.out.println("Please learn Java");
+        }
+    }
+}
