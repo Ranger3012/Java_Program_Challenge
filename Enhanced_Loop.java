f47dda0c2c329207fbb88d1069e9a60b49ec46ef
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2026-02-14 15:26:14 -0500

    Program 24

---
+++ Enhanced_Loop.java
@@ -0,0 +1,13 @@
+class Enhanced_Loop{
+    public static void main(String[] args) {
+        int prime[] = {2, 3, 5, 7, 11};
+        for (int i : prime){
+            System.out.println(i);
+        }
+
+        String lang[] = { "C", "JAVA", "PYTHON"};
+        for(String sample : lang){
+            System.out.println(sample);
+        }
+    }
+}
